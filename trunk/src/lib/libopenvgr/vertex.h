da04fb3803fd725e62a83a92acd876fc6401be99
kenichi.maruyama.0 <kenichi.maruyama.0@0db0628b-1e43-8c53-4057-09391cd66082>
2011-09-15 08:51:29 +0000

    deleted unused functions.

--- trunk/src/lib/libopenvgr/vertex.h
+++ trunk/src/lib/libopenvgr/vertex.h
@@ -16,17 +16,6 @@
 #include "extractFeature.hpp"
 #include "correspondence.hpp"
 
-//! 二次元双曲線データから三次元頂点データを生成
-void HyperbolaToVertex(StereoPairing pairing,  // ステレオペアリング情報
-		       CalibParam calib,       // キャリブレーションデータ
-		       StereoData &stereo,     // ステレオ対応データ
-		       Features2D_old *left,   // 左画像の２次元特徴データ
-		       Features2D_old *right,  // 右画像の２次元特徴データ
-		       unsigned char *edgeL,   // 左画像のエッジ画像
-		       unsigned char *edgeR,   // 右画像のエッジ画像
-		       Parameters parameters   // 全パラメータ
-  );
-
 //! 二次元頂点データから三次元頂点データを生成
 void reconstruct_hyperbola_to_vertex3D(const std::vector<const ovgr::Features2D*> feature,
                                        const ovgr::CorrespondingSet& cs,
