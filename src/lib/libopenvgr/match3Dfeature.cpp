050fb07424edc11547649856cf4b7e147e8f76ab
kawabata.aist@gmail.com <kawabata.aist@gmail.com@0db0628b-1e43-8c53-4057-09391cd66082>
2011-10-03 10:34:20 +0000

    fixed a bug; double free when num == 0.

--- src/lib/libopenvgr/match3Dfeature.cpp
+++ src/lib/libopenvgr/match3Dfeature.cpp
@@ -109,13 +109,13 @@ shrinkMatch3Dresults(Match3Dresults* Match)
         }
       Match->Results[num].n = num;
     }
-  if (num == 0)
-    {
-      return;
-    }
   if ((memory = (MatchResult*) realloc(Match->Results, num * sizeof(MatchResult))) == NULL)
     {
       return;
+    }
+  if (num < 1)
+    {
+      memory = NULL;
     }
 
   //fprintf(stderr, "shrank %d -> %d\n", Match->numOfResults, num);
